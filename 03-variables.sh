be7913e1e2e5ca3837bf674b1add4c6e51755ab3
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-05 10:45:03 +0530

    replacing with variables

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+PERSON1 = Modi
+PERSON2 = Shankar
+
+echo "$PERSON1 : Hi $PERSON2, How are you?"
+echo "$PERSON2 : Hi $PERSON1, I am good. How youh doing?"
+echo "$PERSON1 : I am fine too. What's going on?"
+echo "$PERSON2 : I am going for QUAD meeting. Will you join??"
+echo "$PERSON1 : No, $PERSON2. You carryon, I will be joining next time"
