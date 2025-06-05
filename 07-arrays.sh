d1c74b24594ab2216bf02fd2ff74081411b6ad9a
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-05 11:21:48 +0530

    arrays(movies example)

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,6 +2,6 @@
 
 MOVIES="Court" "HIT" "Thandel"
 
-echo "First Movie is : $MOVIES[0]"
-echo "Last Movie is  : $MOVIES[2]"
-echo "All Movies     : $MOVIES[@]"
+echo "First Movie is : ${MOVIES[0]}"
+echo "Last Movie is  : ${MOVIES[2]}"
+echo "All Movies     : ${MOVIES[@]}"
