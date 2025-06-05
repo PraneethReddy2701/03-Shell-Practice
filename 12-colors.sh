46d31172202a7b35c973e4029e189f09414a8b99
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-05 13:52:47 +0530

    adding colors R,G,Y,N

---
+++ 12-colors.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R=/e[31m
+G=/e[32m
+Y=/e[33m
+N=/e[0m
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error: Run the script with root access $N"
+    exit 1
+else
+    echo -e "You are running with root access"
+fi
+
+VALIDATE()
+{
+    if [ $1 -eq 0 ]
+    then
+        echo -e " Installing $2 is $G SUCCESS $N "
+    else
+        echo -e " Installing $2 is $R FAILURE $N "
+        exit 1
+    fi 
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    dnf install mysql
+    VALIDATE $? "mysql"
+else
+    echo -e "Mysql is already installed.. $Y So SKIPPING $N "
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    dnf install python3
+    VALIDATE $? "python3"
+else
+    echo -e "Python3 is alreaddy installed.. $Y SO SKIPPING $N "
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then 
+    dnf install nginx
+    VALIDATE $? "nginx"
+else
+    echo -e " Nginx is already installed.. $Y So SKIPPING $N "
+fi
