8e5c274354e36f30655c2b1127433e4ecfb1e7aa
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-10 22:05:39 +0530

    set function

--- 16-set.sh
+++ 16-set.sh
@@ -2,4 +2,4 @@
 
 echo "1.hello this will be printing"
 echooo "2.This will not print as it is error"
-echo "3.This also should not print as the above is error"
+echo "3.This also should not print as the above is wrong"
