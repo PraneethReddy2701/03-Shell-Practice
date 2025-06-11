5545e3c98e526837e75e8b8e29db9023b0e696a3
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-11 22:54:19 +0530

    backup-script

--- 18-backup-script.sh
+++ 18-backup-script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 SOURCE_DIR=$1
 DESTINATION_DIR=$2
 DAYS=${3:-14}
 
-if [ $# -lt 2 ]
-then
-    USAGE
-fi
 
 USAGE()
 {
@@ -15,28 +15,36 @@ USAGE()
     exit 1
 }
 
-if [ ! -d $SOURCE_DIR ]
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+
+
+if [ ! -d "$SOURCE_DIR" ]
 then
     echo -e "$R Source Directory does not exist. Please check $N"
     exit 1
 fi
 
-if [ ! -d $DESTINATION_DIR ]
+if [ ! -d "$DESTINATION_DIR" ]
 then
     echo -e "$R Destination Directory does not exist. Please check $N"
     exit 1
 fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+sudo dnf install zip -y
 
 if [ ! -z "$FILES" ]
 then
     echo "Files to zip are : $FILES"
     TIMESTAMP=$(date +%F-%H-%M-%S)
     ZIP_FILE="$DESTINATION_DIR/app-logs-$TIMESTAMP.zip"
-    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
-    
-    if [ -f $ZIP_FILE ]
+    # find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
+    echo "$FILES" | zip -@ "$ZIP_FILE"
+
+    if [ -f "$ZIP_FILE" ]
     then
         echo "Successfully created zip file"
         while IFS= read -r filepath
