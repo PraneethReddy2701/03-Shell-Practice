856387a096ff6752c9ae1b76cc8d236dce0c4456
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-11 22:03:35 +0530

    delete old logs

--- 17-delete-old-logs.sh
+++ 17-delete-old-logs.sh
@@ -2,9 +2,9 @@
 
 SOURCE_DIR=/home/ec2-user/app-logs
 
-FILES_TO_DELETE=$( find $SOURCE_DIR -name "*.log" -mtime +14 )
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
-while IFS= read -r FILEPATH
+while IFS=read -r FILEPATH
 do
     echo $FILEPATH
 done
