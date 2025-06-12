2b9eb3904b367d5ae37b75d1ba536a2d763ee676
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-12 19:38:28 +0530

    disk usage

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -8,13 +8,13 @@ IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 while IFS= read line
 do
     USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
-    PARTITIOn=$(echo $line | awk '{print $7F}')
+    PARTITION=$(echo $line | awk '{print $7F}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        MSG+="High Disk Usage on $PARTITION : $USAGE % <br>"
+        MSG+="High Disk Usage on $PARTITION: $USAGE % <br>"
     fi
 done <<< $DISK_USAGE
 
-# echo -e $MSG
+#echo -e $MSG
 
 sh mail.sh "DevOps Team" "High Disk Usage" "$IP" "$MSG" "praneeth.kotholla@gmail.com" "ALERT-High Disk Usage"
