83758eeab42eec74cded6c7baa9b168c5e24bd0b
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-12 20:58:53 +0530

    read the words in file

--- 22-readfile.sh
+++ 22-readfile.sh
@@ -5,14 +5,29 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-cat $1 | \ #it prints the total content in the file (which we give as argument)
-tr -s '[:punct:]' '[:space]' | \ # tr replaces punctuations and spaces with new lines; -s removes repeated punctutation/space
-tr '[:upper:]' '[:lower:]' | \ # converts all the words to lower
-grep -v '^$' | \ # doesnot includes empty lines in the next input
-sort | \ #sort the list of word alphabetically
-uniq -c | \ # count how many times the word occured
-sort -nr | \ # sort (n) numerically (r) in reverse order
-head -n 5 # to print top 5 words
+# it prints the total content in the file (which we give as argument)
+cat $1 | \ 
+
+# tr replaces punctuations and spaces with new lines; -s removes repeated punctutation/space
+tr -s '[:punct:]' '[:space]' | \ 
+
+# converts all the words to lower
+tr '[:upper:]' '[:lower:]' | \ 
+
+# doesnot includes empty lines in the next input
+grep -v '^$' | \ 
+
+#sort the list of word alphabetically
+sort | \ 
+
+# count how many times the word occured
+uniq -c | \ 
+
+# sort (n) numerically (r) in reverse order
+sort -nr | \ 
+
+# to print top 5 words
+head -n 5 
 
 
 
