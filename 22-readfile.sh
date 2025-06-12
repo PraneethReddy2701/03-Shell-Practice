6feb565b3247d72d5a301eccb2a7203de4367d6a
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-12 21:02:57 +0530

    read the words in file

--- 22-readfile.sh
+++ 22-readfile.sh
@@ -5,35 +5,35 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-# # it prints the total content in the file (which we give as argument)
-# cat "$1" | \ 
+# it prints the total content in the file (which we give as argument)
+cat "$1" | \ 
 
-# # tr replaces punctuations and spaces with new lines; -s removes repeated punctutation/space
-# tr -s '[:punct:]' '[:space]' | \ 
+# tr replaces punctuations and spaces with new lines; -s removes repeated punctutation/space
+tr -s '[:punct:]' '[:space]' | \ 
 
-# # converts all the words to lower
-# tr '[:upper:]' '[:lower:]' | \ 
+# converts all the words to lower
+tr '[:upper:]' '[:lower:]' | \ 
 
-# # doesnot includes empty lines in the next input
-# grep -v '^$' | \ 
+# doesnot includes empty lines in the next input
+grep -v '^$' | \ 
 
-# #sort the list of word alphabetically
-# sort | \ 
+#sort the list of word alphabetically
+sort | \ 
 
-# # count how many times the word occured
-# uniq -c | \ 
+# count how many times the word occured
+uniq -c | \ 
 
-# # sort (n) numerically (r) in reverse order
-# sort -nr | \ 
+# sort (n) numerically (r) in reverse order
+sort -nr | \ 
 
-# # to print top 5 words
-# head -n 5 
+# to print top 5 words
+head -n 5 
 
-cat "$1" | \
-tr -s '[:punct:][:space:]' '\n' | \
-tr '[:upper:]' '[:lower:]' | \
-grep -v '^$' | \
-sort | uniq -c | sort -nr | head -n 5
+# cat "$1" | \
+# tr -s '[:punct:][:space:]' '\n' | \
+# tr '[:upper:]' '[:lower:]' | \
+# grep -v '^$' | \
+# sort | uniq -c | sort -nr | head -n 5
 
 
 
