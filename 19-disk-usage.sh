d61618d620367ef6afddc609ff26578966c7eeef
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-12 19:26:45 +0530

    disk usage

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -11,7 +11,7 @@ do
     PARTITIOn=$(echo $line | awk '{print $7F}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        MSG="High Disk Usage on $PARTITION : $USAGE % <br>
+        MSG+="High Disk Usage on $PARTITION : $USAGE % <br>"
     fi
 done <<< $DISK_USAGE
 
