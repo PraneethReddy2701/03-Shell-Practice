3388b79b955a0afba44a494ead2e6e87196ceef3
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-12 19:48:15 +0530

    cpu utilization

--- 20-cpu-utilization.sh
+++ 20-cpu-utilization.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 
-cpu_usage=$(mpstat 1 1 | awk '/all/{print 100 - $NF}')
+# cpu_usage=$(mpstat 1 1 | awk '/all/{print 100 - $NF}')
+
+# echo "CPU Usage: ${cpu_usage}%"
+
+
+
+cpu_usage=$(mpstat 1 1 | awk '/all/ {print 100 - $NF}')
 
 echo "CPU Usage: ${cpu_usage}%"
