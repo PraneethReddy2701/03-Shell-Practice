85574ffdd5e43c4291a6064660bf2c2cfdae561b
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-12 19:25:14 +0530

    disk usage

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep Filesystem)
+DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1
 MSG=""
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
-while IFS=read -r line
+while IFS= read -r line
 do
     USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
     PARTITIOn=$(echo $line | awk '{print $7F}')
