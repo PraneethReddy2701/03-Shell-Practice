81d2555d49a87c3f10c88743ca046b8fa0a56007
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-06-12 20:54:29 +0530

    read the words in file

---
+++ 22-readfile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+cat $1 | \ #it prints the total content in the file (which we give as argument)
+tr -s '[:punct:]' '[:space]' | \ # tr replaces punctuations and spaces with new lines; -s removes repeated punctutation/space
+tr '[:upper:]' '[:lower:]' | \ # converts all the words to lower
+grep -v '^$' | \ # doesnot includes empty lines in the next input
+sort | \ #sort the list of word alphabetically
+uniq -c | \ # count how many times the word occured
+sort -nr | \ # sort (n) numerically (r) in reverse order
+head -n 5 # to print top 5 words
+
+
+
+
+
+
+
